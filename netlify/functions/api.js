739b56ec5208b2b7b62b8d3344f505a432d04136
Anna Jones <annamariajones@live.co.uk>
2025-01-23 17:13:24 +0100

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -46,7 +46,9 @@ app.use(express.json())
 app.use(express.static("public"));
 
 
-app.set('views', path.join(process.cwd(), 'views'));
+app.set('views','views');
+
+app.set("view engine", "ejs")
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'))
